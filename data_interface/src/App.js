6f44f236c1a5750585146712a4d7d5c7c689b665
Robert Charca <trevoraobac@gmail.com>
2022-10-31 19:40:10 -0500

    React project created with a simple chart using Django API

---
+++ data_interface/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ShowingData from './chart_django/ShowingData';
+
+function App() {
+  return (
+    <div className="App">
+      <ShowingData />
+    </div>
+  );
+}
+
+export default App;
