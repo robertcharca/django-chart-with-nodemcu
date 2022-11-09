d525cb9a5aeb50b9b9f510311623d9018b1f53e3
Robert Charca <trevoraobac@gmail.com>
2022-11-09 17:13:46 -0500

    Lineal chart applied

--- data_interface/src/chart_django/ShowingData.jsx
+++ data_interface/src/chart_django/ShowingData.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Chart from "chart.js/auto";
-import { Chart as Chartjs, BarElement, CategoryScale, LinearScale} from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { Chart as Chartjs, LinearScale} from "chart.js";
+import { Line } from "react-chartjs-2";
 
 Chartjs.register(
-    CategoryScale,
     LinearScale,
-    BarElement
 )
 
 var apiUrl = "http://127.0.0.1:8000/sensorvalues/"
@@ -24,23 +22,13 @@ const ShowingData = () => {
     const [data, setData] = useState({
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
+            label: 'Values',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(75,192,192,1)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(0,0,0,1)',
             ],
             borderWidth: 1
         }]
@@ -69,23 +57,13 @@ const ShowingData = () => {
                 setData({
                     labels: dates,
                     datasets: [{
-                        label: 'quantity',
+                        label: 'value',
                         data: values,
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
+                            'rgba(75,192,192,1)',
                         ],
                         borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
+                            'rgba(0, 180, 216)',
                         ],
                         borderWidth: 1
                     }]
@@ -99,7 +77,7 @@ const ShowingData = () => {
 
     return(
         <div>
-            <Bar data={data} height={200} options={options}/>
+            <Line data={data} height={200} options={options}/>
         </div>
     )
 }
